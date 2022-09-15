767ef8027f76aef79535dc4c4c7883cf04c42eb9
Joey Kraut <joey@otter.cash>
2022-09-15 15:39:21 -0700

    beaver: Add SharedValueSource trait and dummy implementation
    The SharedValueSource is intended to be implemented outside of
    the package, as this is an infrastructure concern.
    
    The dummy implementation is used for testing.

---
+++ src/beaver.rs
@@ -0,0 +1,35 @@
+//! Defines the Beaver value generation interface
+//! as well as a dummy beaver interface for testing
+
+use curve25519_dalek::scalar::Scalar;
+
+/// SharedValueSource implements both the functionality for:
+///     1. Single additively shared values [x] where party 1 holds
+///        x_1 and party 2 holds x_2 such that x_1 + x_2 = x
+///     2. Beaver triplets; additively shared values [a], [b], [c] such
+///        that a * b = c
+pub trait SharedValueSource<T> {
+    // Fetch the next shared single value
+    fn next_shared_value(&mut self) -> T;
+    // Fetch the next beaver triplet
+    fn next_triplet(&mut self) -> (T, T, T);
+}
+
+/// A dummy value source that outputs only ones
+/// Used for testing
+pub(crate) struct DummySharedScalarSource;
+
+#[allow(dead_code)]
+impl DummySharedScalarSource {
+    pub fn new() -> Self { Self }
+}
+
+impl SharedValueSource<Scalar> for DummySharedScalarSource {
+    fn next_shared_value(&mut self) -> Scalar {
+        Scalar::one()
+    } 
+
+    fn next_triplet(&mut self) -> (Scalar, Scalar, Scalar) {
+        (Scalar::one(), Scalar::one(), Scalar::one())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod beaver;
 pub mod error;
 pub mod mpc_scalar;
 pub mod network;
