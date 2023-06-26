c631f43ceb92836f2bee3f44a962f29a9d190ca0
Joey Kraut <joey@renegade.fi>
2023-06-26 10:35:51 -0700

    network: Refactor interface ahead of OOO implementation

--- src/network/dummy_network.rs
+++ src/network/dummy_network.rs
@@ -8,86 +8,34 @@ use curve25519_dalek::{ristretto::RistrettoPoint, scalar::Scalar};
 
 use crate::error::MpcNetworkError;
 
-use super::MpcNetwork;
+use super::{MpcNetwork, NetworkOutbound};
 
 #[derive(Clone, Debug, Default)]
 pub struct DummyMpcNetwork {
-    /// Append to mock a scalar sent from a peer
-    mock_scalars: Vec<Scalar>,
-    /// Append to mock a Ristretto point sent from a peer
-    mock_points: Vec<RistrettoPoint>,
+    /// A list of mock messages sent from the peer
+    mock_messages: Vec<NetworkOutbound>,
 }
 
 impl DummyMpcNetwork {
     pub fn new() -> Self {
         Self {
-            mock_scalars: vec![],
-            mock_points: vec![],
+            mock_messages: Vec::new(),
         }
     }
 
-    pub fn add_mock_scalars(&mut self, scalars: Vec<Scalar>) {
-        self.mock_scalars.extend_from_slice(&scalars);
-    }
-
-    pub fn add_mock_points(&mut self, points: Vec<RistrettoPoint>) {
-        self.mock_points.extend_from_slice(&points);
+    pub fn add_mock_messages(&mut self, messages: Vec<NetworkOutbound>) {
+        self.mock_messages.extend(messages);
     }
 }
 
 #[async_trait]
 impl MpcNetwork for DummyMpcNetwork {
-    /// Always return king
-    fn party_id(&self) -> u64 {
-        0
-    }
-
-    async fn send_bytes(&mut self, _: &[u8]) -> Result<(), MpcNetworkError> {
+    async fn send_message(&mut self, message: NetworkOutbound) -> Result<(), MpcNetworkError> {
         Ok(())
     }
 
-    async fn receive_bytes(&mut self) -> Result<Vec<u8>, MpcNetworkError> {
-        Err(MpcNetworkError::RecvError)
-    }
-
-    async fn send_scalars(&mut self, _: &[Scalar]) -> Result<(), MpcNetworkError> {
-        Ok(())
-    }
-
-    async fn receive_scalars(
-        &mut self,
-        num_scalars: usize,
-    ) -> Result<Vec<Scalar>, MpcNetworkError> {
-        Ok(self.mock_scalars.drain(0..num_scalars).as_slice().to_vec())
-    }
-
-    async fn broadcast_points(
-        &mut self,
-        points: &[RistrettoPoint],
-    ) -> Result<Vec<RistrettoPoint>, MpcNetworkError> {
-        Ok(self.mock_points.drain(0..points.len()).as_slice().to_vec())
-    }
-
-    async fn send_points(&mut self, _: &[RistrettoPoint]) -> Result<(), MpcNetworkError> {
-        Ok(())
-    }
-
-    async fn receive_points(
-        &mut self,
-        num_points: usize,
-    ) -> Result<Vec<RistrettoPoint>, MpcNetworkError> {
-        Ok(self.mock_points.drain(0..num_points).as_slice().to_vec())
-    }
-
-    async fn broadcast_scalars(
-        &mut self,
-        scalars: &[Scalar],
-    ) -> Result<Vec<Scalar>, MpcNetworkError> {
-        Ok(self
-            .mock_scalars
-            .drain(0..scalars.len())
-            .as_slice()
-            .to_vec())
+    async fn receive_message(&mut self) -> Result<NetworkOutbound, MpcNetworkError> {
+        Ok(self.mock_messages.pop().expect("mock messages exhausted"))
     }
 
     async fn close(&mut self) -> Result<(), MpcNetworkError> {
