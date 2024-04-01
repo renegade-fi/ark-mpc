c751b3cbb4db1086bbbd2045ee9ef9ab71871fe6
Joey Kraut <joey@renegade.fi>
2024-04-01 15:40:30 -0700

    mp-spdz-rs: build: Link full FHE library into project and test methods

--- mp-spdz-rs/README.md
+++ mp-spdz-rs/README.md
@@ -2,8 +2,8 @@
 1. Run `git submodule update --init --recursive` to clone the submodules in this repo.
 2. Install `libsodium` & `gmp` via:
 ```zsh
-brew install libsodium gmp boost ntl
-yum install libsodium gmp boost ntl
-apt-get install libsodium gmp boost ntl
+brew install libsodium gmp boost ntl openssl
+yum install libsodium gmp boost ntl openssl
+apt-get install libsodium gmp boost ntl openssl
 ```
 based on your local package manager.
--- mp-spdz-rs/src/lib.rs
+++ mp-spdz-rs/src/lib.rs
@@ -7,10 +7,18 @@
 #[cxx::bridge]
 pub mod ffi {
     unsafe extern "C++" {
-        include!("FHE/Ring.h");
+        include!("FHE/FHE_Params.h");
+        include!("Math/bigint.h");
 
-        type Ring;
-        fn new_ring(m: i32) -> UniquePtr<Ring>;
+        // `bigint`
+        type bigint;
+        fn print(self: &bigint);
+
+        // `FHE_Params`
+        type FHE_Params;
+        fn new_fhe_params(n_mults: i32, drown_sec: i32) -> UniquePtr<FHE_Params>;
+        fn basic_generation_mod_prime(self: Pin<&mut FHE_Params>, plaintext_length: i32);
+        fn get_plaintext_mod(params: &FHE_Params) -> UniquePtr<bigint>;
     }
 }
 
@@ -20,6 +28,10 @@ mod test {
 
     #[test]
     fn test_dummy() {
-        let ring = new_ring(10);
+        let mut params = new_fhe_params(0 /* mults */, 128 /* sec */);
+        params.pin_mut().basic_generation_mod_prime(255 /* bitlength */);
+
+        let plaintext_modulus = get_plaintext_mod(&params);
+        plaintext_modulus.print();
     }
 }
